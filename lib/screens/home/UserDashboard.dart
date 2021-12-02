479bf22fe6dcd12ae21ea9e70bb317faf5bdc407
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2021-12-02 10:05:58 +0000

    Images fixed by adding extra assets locations in pubspec.yaml. Added Stying. Added API may need debugging depending on if its an emulator issue

--- lib/screens/home/UserDashboard.dart
+++ lib/screens/home/UserDashboard.dart
@@ -3,9 +3,39 @@ import 'package:meditationapp/screens/home/features/ambientsounds.dart';
 import 'package:meditationapp/screens/home/features/moodslogs.dart';
 import 'package:meditationapp/screens/home/features/supportpage.dart';
 import 'package:meditationapp/services/auth.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import 'features/removalads.dart';
+
+Future<Quote> fetchQuote() async {
+  final response = await http.get(Uri.parse('http://quotes.rest/qod.json?category=inspire'));
+
+  if (response.statusCode == 200) {
+
+    return Quote.fromJson(jsonDecode(response.body));
+  } else {
+
+    throw Exception('Failed to load Quote of the Day');
+  }
+}
+
+class Quote {
+  final String quote;
+  final String author;
+
+  Quote({
+    required this.quote,
+    required this.author,
+  });
+
+  factory Quote.fromJson(Map<String, dynamic> json) {
+    return Quote(
+      quote: json['quote'],
+      author: json['author']
+    );
+  }
+}
 
 class UserDashboard extends StatefulWidget {
   @override
@@ -13,12 +43,23 @@ class UserDashboard extends StatefulWidget {
 }
 
 class _UserDashboardState extends State<UserDashboard> {
+  late Future<Quote> futureQuote;
+
+  @override
+  void initState() {
+    super.initState();
+    futureQuote = fetchQuote();
+  }
+
+  String url = 'http://quotes.rest/qod.json?category=inspire';
 
   final AuthService _auth = AuthService();
 
 
   @override
   Widget build(BuildContext context) {
+
+    var cardStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
 
     return Scaffold(
       appBar: AppBar(
@@ -46,8 +87,26 @@ class _UserDashboardState extends State<UserDashboard> {
           ),
         ),
       ),
+
+            Container(
+              alignment: Alignment.topCenter,
+                padding: const EdgeInsets.only(top: 75),
+                child: FutureBuilder<Quote>(
+                  future: futureQuote,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Text('${snapshot.data!.quote}');
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
+
+                    return const CircularProgressIndicator();
+                  }
+                ),
+            ),
+
           Padding(
-            padding: const EdgeInsets.only(top: 50, left: 10, right: 10),
+            padding: const EdgeInsets.only(top: 150, left: 10, right: 10),
             child: GridView.count(
               mainAxisSpacing: 10,
               crossAxisCount: 2,
@@ -70,8 +129,8 @@ class _UserDashboardState extends State<UserDashboard> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         // TODO Find why images wont be displayed in userdashboard directory
-                        Image.asset('assets/images/userdashboard/audio-wavesPNG.png'),
-                        Text('Ambient Sounds')
+                        Image.asset('assets/images/userdashboard/audiowaves.png', height: 100),
+                        Text('Ambient Sounds', style: cardStyle)
                       ],
                     ),
                   ),
@@ -91,8 +150,8 @@ class _UserDashboardState extends State<UserDashboard> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Image.asset('assets/images/userdashboard/moodPNG.png'),
-                        Text('Mood Logs')
+                        Image.asset('assets/images/userdashboard/mood.png', height: 100),
+                        Text('Mood Logs', style: cardStyle)
                       ],
                     ),
                   ),
@@ -112,8 +171,8 @@ class _UserDashboardState extends State<UserDashboard> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Image.asset('assets/images/userdashboard/supportPNG.png'),
-                        Text('Extra Support')
+                        Image.asset('assets/images/userdashboard/support.png', height: 100),
+                        Text('Extra Support', style: cardStyle)
                       ],
                     ),
                   ),
@@ -133,8 +192,8 @@ class _UserDashboardState extends State<UserDashboard> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Image.asset('assets/images/userdashboard/delete.PNG.png'),
-                        Text('Advert Removal')
+                        Image.asset('assets/images/userdashboard/delete.png', height: 100),
+                        Text('Advert Removal', style: cardStyle)
                       ],
                     ),
                   ),
