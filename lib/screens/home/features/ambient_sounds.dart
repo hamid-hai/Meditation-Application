a913ec0b74a8a248b58b5c065d37ed7e86e47cb7
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2022-01-02 19:00:47 +0000

    ambient_sounds.dart class formatted to follow coding standards for Flutter

--- lib/screens/home/features/ambient_sounds.dart
+++ lib/screens/home/features/ambient_sounds.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
-
-
+import 'package:flutter/material.dart';
 
 class Sounds extends StatefulWidget {
   const Sounds({Key? key}) : super(key: key);
@@ -11,8 +9,7 @@ class Sounds extends StatefulWidget {
 }
 
 class _SoundsState extends State<Sounds> {
-
-  AudioPlayer audioPlayer = new AudioPlayer();
+  AudioPlayer audioPlayer = AudioPlayer();
 
   bool playing = false;
 
@@ -20,25 +17,24 @@ class _SoundsState extends State<Sounds> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Ambient Sounds"),
+        title: const Text("Ambient Sounds"),
         backgroundColor: Colors.deepPurple,
       ),
-
       body: Stack(
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.only(top: 20),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Tap to play an amibent sound\nTap again to pause\n(An internet connection is required)', textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-
+              children: const [
+                Text(
+                    'Tap to play an amibent sound\nTap again to pause\n(An internet connection is required)',
+                    textAlign: TextAlign.center,
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
               ],
             ),
           ),
-
-
           Padding(
             padding: const EdgeInsets.only(top: 100, left: 10, right: 10),
             child: GridView.count(
@@ -51,11 +47,10 @@ class _SoundsState extends State<Sounds> {
                   // REFERENCE: https://api.flutter.dev/flutter/material/InkWell-class.html
                   child: InkWell(
                     onTap: () {
-                      print('Rain tapped');
-                      getAudio('https://cdn.staticcrate.com/stock-hd/audio/SOUNDSCRATE-RainyDay.mp3');
+                      getAudio(
+                          'https://cdn.staticcrate.com/stock-hd/audio/SOUNDSCRATE-RainyDay.mp3');
                     },
                     onLongPress: () {
-                      print('rain long tapped');
                       // REFERENCE https://stackoverflow.com/a/45948243
                       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                         content: Text("A relaxing rain sound"),
@@ -67,24 +62,20 @@ class _SoundsState extends State<Sounds> {
                         Image.network(
                             'https://cdn.iconscout.com/icon/free/png-256/cloud-rain-1895139-1605055.png',
                             height: 100),
-                        Text('Rain')
+                        const Text('Rain')
                       ],
                     ),
                   ),
                 ),
-
-
                 Card(
                   elevation: 4,
                   // REFERENCE: https://api.flutter.dev/flutter/material/InkWell-class.html
                   child: InkWell(
                     onTap: () {
-                      print('Beach waves tapped');
                       getAudio(
                           'https://cdn.staticcrate.com/stock-hd/audio/soundscrate-loopable-waves-on-beach-windy-1.mp3');
                     },
                     onLongPress: () {
-                      print('Beach Waves long tapped');
                       // REFERENCE https://stackoverflow.com/a/45948243
                       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                         content: Text("A relaxing rain sound"),
@@ -96,24 +87,20 @@ class _SoundsState extends State<Sounds> {
                         Image.network(
                             'https://icon-library.com/images/air-element-icon/air-element-icon-8.jpg',
                             height: 100),
-                        Text('Beach Waves')
+                        const Text('Beach Waves')
                       ],
                     ),
                   ),
                 ),
-
-
                 Card(
                   elevation: 4,
                   // REFERENCE: https://api.flutter.dev/flutter/material/InkWell-class.html
                   child: InkWell(
                     onTap: () {
-                      print('Subway tapped');
                       getAudio(
                           'https://cdn.staticcrate.com/stock-hd/audio/soundscrate-public-subway-interior-1.mp3');
                     },
                     onLongPress: () {
-                      print('Subway long tapped');
                       // REFERENCE https://stackoverflow.com/a/45948243
                       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                         content: Text("A relaxing subway sound"),
@@ -125,24 +112,20 @@ class _SoundsState extends State<Sounds> {
                         Image.network(
                             'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/BSicon_TRAIN2.svg/1024px-BSicon_TRAIN2.svg.png',
                             height: 100),
-                        Text('Subway')
+                        const Text('Subway')
                       ],
                     ),
                   ),
                 ),
-
-
                 Card(
                   elevation: 4,
                   // REFERENCE: https://api.flutter.dev/flutter/material/InkWell-class.html
                   child: InkWell(
                     onTap: () {
-                      print('City tapped');
                       getAudio(
                           'https://cdn.staticcrate.com/stock-hd/audio/soundscrate-city-ambience-4.mp3');
                     },
                     onLongPress: () {
-                      print('rain long tapped');
                       // REFERENCE https://stackoverflow.com/a/45948243
                       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                         content: Text("A relaxing city sound"),
@@ -154,7 +137,7 @@ class _SoundsState extends State<Sounds> {
                         Image.network(
                             'https://cdn-icons-png.flaticon.com/512/699/699421.png',
                             height: 100),
-                        Text('City')
+                        const Text('City')
                       ],
                     ),
                   ),
@@ -164,12 +147,10 @@ class _SoundsState extends State<Sounds> {
                   // REFERENCE: https://api.flutter.dev/flutter/material/InkWell-class.html
                   child: InkWell(
                     onTap: () {
-                      print('Fire tapped');
                       getAudio(
                           'https://cdn.staticcrate.com/stock-hd/audio/soundscrate-fire.mp3');
                     },
                     onLongPress: () {
-                      print('fire long tapped');
                       // REFERENCE https://stackoverflow.com/a/45948243
                       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                         content: Text("A relaxing fire sound"),
@@ -181,7 +162,7 @@ class _SoundsState extends State<Sounds> {
                         Image.network(
                             'http://simpleicon.com/wp-content/uploads/fire-256x256.png',
                             height: 100),
-                        Text('Fire')
+                        const Text('Fire')
                       ],
                     ),
                   ),
@@ -191,12 +172,10 @@ class _SoundsState extends State<Sounds> {
                   // REFERENCE: https://api.flutter.dev/flutter/material/InkWell-class.html
                   child: InkWell(
                     onTap: () {
-                      print('Forest tapped');
                       getAudio(
                           'https://cdn.staticcrate.com/stock-hd/audio/soundscrate-forest-ambient.mp3');
                     },
                     onLongPress: () {
-                      print('forest long tapped');
                       // REFERENCE https://stackoverflow.com/a/45948243
                       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                         content: Text("A relaxing forest sound"),
@@ -208,7 +187,7 @@ class _SoundsState extends State<Sounds> {
                         Image.network(
                             'https://www.pngfind.com/pngs/m/127-1272126_christmas-trees-comments-forest-icon-png-black-and.png',
                             height: 100),
-                        Text('Forest')
+                        const Text('Forest')
                       ],
                     ),
                   ),
@@ -221,34 +200,24 @@ class _SoundsState extends State<Sounds> {
     );
   }
 
-  void getAudio(String URL) async {
-
-    if(playing){
-
-      print('paused');
-
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text("Ambient Sound paused")));
-
+  void getAudio(String url) async {
+    if (playing) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text("Ambient Sound paused")));
 
       var res = await audioPlayer.pause();
-      if(res == 1){
+      if (res == 1) {
         setState(() {
           playing = false;
         });
       }
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text("Ambient Sound playing")));
 
-    }else{
+      var res = await audioPlayer.play(url, isLocal: false);
 
-      print('playing');
-
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text("Ambient Sound playing")));
-
-      
-      var res = await audioPlayer.play(URL, isLocal: false);
-
-      if(res == 1){
+      if (res == 1) {
         setState(() {
           playing = true;
         });
