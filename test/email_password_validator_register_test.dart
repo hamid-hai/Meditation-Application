f94371bc37d06876539dcab7e155101cf0e7a5d4
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2022-01-02 10:40:57 +0000

    email_password_validator_register_test.dart file created testing nullability and validation of the register screen

--- test/email_password_validator_register_test.dart
+++ test/email_password_validator_register_test.dart
@@ -1,29 +1,30 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:meditationapp/screens/authenticate/sign_in.dart';
+import 'package:meditationapp/screens/authenticate/register.dart';
+
 
 void main() {
 
   test('empty email returns error string', () {
 
-    var result = EmailFieldValidator.validate('');
+    var result = EmailFieldValidatorRegister.validate('');
     expect(result, 'Please enter a valid email');
   });
 
   test('non-empty email returns null', () {
 
-    var result = EmailFieldValidator.validate('email');
+    var result = EmailFieldValidatorRegister.validate('email');
     expect(result, null);
   });
 
   test('empty password returns error string', () {
 
-    var result = PasswordFieldValidator.validate('');
+    var result = PasswordFieldValidatorRegister.validate('');
     expect(result, 'Enter a strong password (6+ characters)');
   });
 
   test('non-empty password returns null', () {
 
-    var result = PasswordFieldValidator.validate('password');
+    var result = PasswordFieldValidatorRegister.validate('password');
     expect(result, null);
   });
 }
