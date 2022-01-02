a0a0775e65010dd02ae60f1b634b59194140e8a6
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2022-01-02 19:00:22 +0000

    moodlogs directory formatted to follow coding standards for Flutter

--- lib/screens/home/features/mood_logs.dart
+++ lib/screens/home/features/mood_logs.dart
@@ -36,154 +36,147 @@ class _MoodsLogsState extends State<MoodsLogs> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Mood Logs"),
+        title: const Text("Mood Logs"),
         backgroundColor: Colors.deepPurple,
       ),
-      body: Container(
-        child: Column(children: <Widget>[
-          SizedBox(width: 70),
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: Text(
-                'Select a date and time\nPress the tick button to confirm',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton.icon(
-                    icon: Icon(Icons.date_range),
-                    label: Text('Pick a date'),
-                    onPressed: () => showDatePicker(
-
-                      // Calculating the date the user has pressed
-                      // Converting to a string format for the 'Mood Logs History'
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime(2001),
-                            lastDate: DateTime(2022))
-                        .then((date) => {
-                              setState(() {
-                                datesorter = date!;
-                                datepicked = date!.day.toString() +
-                                    '-' +
-                                    date.month.toString() +
-                                    '-' +
-                                    date.year.toString();
-                                dateonly = date.day.toString() +
-                                    '/' +
-                                    date.month.toString();
-                              }),
-                            })),
-
-                // Repeating the process for the Time
-                TextButton.icon(
-                    icon: Icon(Icons.timer),
-                    label: Text('Pick a Time'),
-                    onPressed: () => showTimePicker(
-                            context: context, initialTime: TimeOfDay.now())
-                        .then((time) => {
-                              setState(() {
-                                timepicked = time!.format(context).toString();
-                              })
-                            })),
-
-
-                // By pressing confirm we combine the date and time into one string
-                Container(
-                  height: 40,
-                  width: 40,
-                  child: FloatingActionButton(
-                    child: Icon(Icons.done),
-                    onPressed: () => setState(() {
-                      datetime = datepicked + '  ' + timepicked;
-                      print(datetime.toString());
-                    }),
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-
-          SizedBox(height: 50),
-          Text('How are you feeling?',
+      body: Column(children: <Widget>[
+        const SizedBox(width: 70),
+        const Padding(
+          padding: EdgeInsets.only(top: 8.0),
+          child: Text(
+              'Select a date and time\nPress the tick button to confirm',
+              textAlign: TextAlign.center,
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          SizedBox(height: 6),
-          Text(
-            '(Select a mood from below)\n\n(Tap to select, tap again to deselect)',
-            textAlign: TextAlign.center,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton.icon(
+                  icon: const Icon(Icons.date_range),
+                  label: const Text('Pick a date'),
+                  onPressed: () => showDatePicker(
+
+                          // Calculating the date the user has pressed
+                          // Converting to a string format for the 'Mood Logs History'
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2001),
+                          lastDate: DateTime(2022))
+                      .then((date) => {
+                            setState(() {
+                              datesorter = date!;
+                              datepicked = date.day.toString() +
+                                  '-' +
+                                  date.month.toString() +
+                                  '-' +
+                                  date.year.toString();
+                              dateonly = date.day.toString() +
+                                  '/' +
+                                  date.month.toString();
+                            }),
+                          })),
+
+              // Repeating the process for the Time
+              TextButton.icon(
+                  icon: const Icon(Icons.timer),
+                  label: const Text('Pick a Time'),
+                  onPressed: () => showTimePicker(
+                          context: context, initialTime: TimeOfDay.now())
+                      .then((time) => {
+                            setState(() {
+                              timepicked = time!.format(context).toString();
+                            })
+                          })),
+
+              // By pressing confirm we combine the date and time into one string
+              SizedBox(
+                height: 40,
+                width: 40,
+                child: FloatingActionButton(
+                  child: const Icon(Icons.done),
+                  onPressed: () => setState(() {
+                    datetime = datepicked + '  ' + timepicked;
+                  }),
+                ),
+              ),
+            ],
           ),
+        ),
 
+        const SizedBox(height: 50),
+        const Text('How are you feeling?',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        const SizedBox(height: 6),
+        const Text(
+          '(Select a mood from below)\n\n(Tap to select, tap again to deselect)',
+          textAlign: TextAlign.center,
+        ),
 
-          Expanded(
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: moods.length,
-                itemBuilder: (context, index) {
-                  return Row(
-                    children: <Widget>[
-                      Center(child: SizedBox(width: 20)),
-                      GestureDetector(
-                          child: MoodIcon(
-                              image: moods[index].moodimage,
-                              name: moods[index].name,
-                              colour: moods[index].iselected
-                                  ? Colors.black
-                                  : Colors.white),
-                          onTap: () => {
-                                if (ontapcount == 0)
-                                  {
-                                    setState(() {
-                                      mood = moods[index].name;
-                                      image = moods[index].moodimage;
-                                      moods[index].iselected = true;
-                                      ontapcount = ontapcount + 1;
-                                      print(mood);
-                                    }),
-                                  }
-                                else if (moods[index].iselected)
-                                  {
-                                    setState(() {
-                                      moods[index].iselected = false;
-                                      ontapcount = 0;
-                                    })
-                                  }
-                              }),
-                    ],
-                  );
-                }),
-          ),
+        Expanded(
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: moods.length,
+              itemBuilder: (context, index) {
+                return Row(
+                  children: <Widget>[
+                    const Center(child: SizedBox(width: 20)),
+                    GestureDetector(
+                        child: MoodIcon(
+                            image: moods[index].moodimage,
+                            name: moods[index].name,
+                            colour: moods[index].iselected
+                                ? Colors.black
+                                : Colors.white),
+                        onTap: () => {
+                              if (ontapcount == 0)
+                                {
+                                  setState(() {
+                                    mood = moods[index].name;
+                                    image = moods[index].moodimage;
+                                    moods[index].iselected = true;
+                                    ontapcount = ontapcount + 1;
+                                  }),
+                                }
+                              else if (moods[index].iselected)
+                                {
+                                  setState(() {
+                                    moods[index].iselected = false;
+                                    ontapcount = 0;
+                                  })
+                                }
+                            }),
+                  ],
+                );
+              }),
+        ),
 
-          // Async function that contacts firebase and stores the data collected on this screen.
-          TextButton.icon(
-            icon: Icon(Icons.send),
-            label: Text('Submit'),
-            onPressed: () async {
-              User? user = FirebaseAuth.instance.currentUser;
+        // Async function that contacts firebase and stores the data collected on this screen.
+        TextButton.icon(
+          icon: const Icon(Icons.send),
+          label: const Text('Submit'),
+          onPressed: () async {
+            User? user = FirebaseAuth.instance.currentUser;
 
-              await FirebaseFirestore.instance.collection('diaries').add({
-                'uid': user!.uid,
-                'dateandtime': datetime,
-                'date': datesorter,
-                'mood': mood
-              });
+            await FirebaseFirestore.instance.collection('diaries').add({
+              'uid': user!.uid,
+              'dateandtime': datetime,
+              'date': datesorter,
+              'mood': mood
+            });
+          },
+        ),
+
+        // Pushes to next screen, Mood Logs History
+        TextButton.icon(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const MoodHistory()));
             },
-          ),
-
-          // Pushes to next screen, Mood Logs History
-          TextButton.icon(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => moodHistory()));
-              },
-              icon: Icon(Icons.table_rows_rounded),
-              label: Text('Go to Mood Logs History')),
-        ]),
-      ),
+            icon: const Icon(Icons.table_rows_rounded),
+            label: const Text('Go to Mood Logs History')),
+      ]),
     );
   }
 }
--- lib/screens/home/features/moodlogs/mood_icon.dart
+++ lib/screens/home/features/moodlogs/mood_icon.dart
@@ -4,13 +4,27 @@ class MoodIcon extends StatelessWidget {
   String image;
   String name;
   Color colour;
-  MoodIcon({required this.name,required this.image,required this.colour});
+
+  MoodIcon(
+      {Key? key, required this.name, required this.image, required this.colour})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Column(children:<Widget>[Image.asset(image,height: 50),Text(name, textAlign: TextAlign.center,)],),
-      decoration: BoxDecoration(border:Border.all(color: colour),),
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Image.asset(image, height: 50),
+          Text(
+            name,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(color: colour),
+      ),
       height: 100,
-
     );
   }
 }
